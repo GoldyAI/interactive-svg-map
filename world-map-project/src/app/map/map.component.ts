a7e0a387da4abb953a12ba1ae27ed3baa2378383
GoldyAI <139739324+GoldyAI@users.noreply.github.com>
2025-02-12 22:49:50 -0600

    Step C: Integrated World Bank API and displayed six country properties

--- world-map-project/src/app/map/map.component.ts
+++ world-map-project/src/app/map/map.component.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgIf, CurrencyPipe } from '@angular/common'; 
+import { CountryService } from '../services/country.service';
 
 @Component({
   selector: 'app-map',
-  standalone: true,  
+  standalone: true,
+  imports: [CommonModule, NgIf, CurrencyPipe], 
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+  providers: [CountryService] 
 })
-export class MapComponent { }
+export class MapComponent {
+  countryInfo: any = null; 
+
+  constructor(private countryService: CountryService) {}
+
+  // Fetch country data when a country is clicked
+  fetchCountryData(countryCode: string) {
+    this.countryService.getCountryData(countryCode).subscribe(
+      (data: any) => {  
+        if (data && data[1]) {
+          this.countryInfo = {
+            name: data[1][0].name,               
+            capital: data[1][0].capitalCity,     
+            region: data[1][0].region.value,     
+            incomeLevel: data[1][0].incomeLevel.value, 
+            population: data[1][0].population,   
+            gdp: data[1][0].gdp                 
+          };
+        }
+      },
+      (error: any) => { 
+        console.error('Error fetching country data:', error);
+      }
+    );
+  }
+}
